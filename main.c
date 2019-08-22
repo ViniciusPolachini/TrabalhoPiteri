9a60756348000aaa6c172a700b1eb5dbe3477379
ViniciusPolachini <vipolachini@uol.com.br>
2019-08-22 14:42:51 -0300

    Operação 10 e Arrays
    Programei a operação 10 para podermos checar se outras funções estão funcionando, como a de gerar números randomicamente.

--- main.c
+++ main.c
@@ -5,6 +5,17 @@
 #define MAX 100
 #define QUEBRA "\n\n\n"
 #define OPCOES "\n[1] Intersecção entre A e B \n[2] União entre A e B \n[3] Diferença A menos B \n[4] Diferença B menos A \n[5] Verificar se um elemento arbitrário pertence ao conjunto \n[6] Verificar se há elemtos repetidos em A ou B \n[7] Eliminar elementos repitidos de um conjunto \n[8] Verificar se um conjunto é vazio\n[9] Determinar a quantidade de elementos do conjunto\n[10] Visualizar em qualquer momeno os conjuntos envolvidos A e B e os conjuntos resultantes \n\nEscolha uma operação:"
+void Introduzido();
+void Ordenacao();
+void Interseccao();
+void Uniao();
+void Diferenca();
+void Arbitrario();
+void Repetidos();
+void Elimina();
+void Vazio();
+void Quantidade();
+void Visualizar(int *ptrA, int *ptrB);
 
 void Bubble_Sort (int *A, int Dim)
 {
@@ -45,23 +56,17 @@ void Random(int *A, int Dim)
  return;
 }
 
-
-void Introduzido();
-void Ordenacao();
-void Interseccao();
-void Uniao();
-void Diferenca();
-void Arbitrario();
-void Repetidos();
-void Elimina();
-void Vazio();
-void Quantidade();
-void Visualizar();
+void Visualizar(int *ptrA, int *ptrB){
+    int i;
+    for(i=0; i<=MAX-1; i++){
+        printf("%d////%d\n", *(ptrA+i), *(ptrB+i));
+    }
+}
 
 int main(void)
 {
     setlocale(LC_ALL,"portuguese");
-    int x, y;
+    int x, y, VA[MAX],VB[MAX],VC[MAX*2];
     do{
     system("cls");
     printf(" ####### Trabalho prático de programação 1 - ATP 2 #######");
@@ -74,12 +79,12 @@ int main(void)
     }while(x<1 || x>3);
 
     switch(x){
-    case 1: do{system("cls");puts(OPCOES);scanf("%d", &y);}while(y<1 || y>10);break;
+    case 1: do{Random(VA,MAX);Random(VB,MAX);system("cls");puts(OPCOES);scanf("%d", &y);}while(y<1 || y>10);break;
     case 2: do{system("cls");puts(OPCOES);scanf("%d", &y);}while(y<1 || y>10);break;
     case 3: return 0;
     }
     switch(y){
-
+    case 10: printf("A////B\n"); Visualizar(VA, VB); break;
     }
     return 0;
 }
