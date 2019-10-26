ff271ec578b5cb030e11d578a12d9a4701ab22b5
ViniciusPolachini <vipolachini@uol.com.br>
2019-10-26 17:22:48 -0300

    Matriz par n multipla de 4

--- tpp-03.c
+++ tpp-03.c
@@ -125,7 +125,201 @@ void matriz_mult_4(int ordem){
 /* Gerar matriz mágica de ordem par */
 
 void matriz_par(int ordem){
-    /*função*/
+    int matriz[ordem][ordem],aux=(ordem*ordem)/4,sup=ordem, aux2=ordem/2, aux3 ,i=0, j=ordem/4, k, l, num=1;
+    
+     for(k = 0; k < ordem; k++){
+        for (l = 0; l < ordem; l++){
+            matriz[k][l] = 0;
+        }
+    }
+
+    while(num<=aux){
+        matriz[i][j]=num;
+        num++;
+        if(i-1>=0 && j+1<aux2){
+            if(matriz[i-1][j+1]==0){
+                i--;
+                j++;
+            }
+            else{
+                i++;
+            }
+        }
+            else if(i-1>=0){
+                if(matriz[i-1][0]==0){
+                   j=0;
+                   i--;
+                }
+                else{
+                    i++;
+                }
+            } 
+                else if(j+1<aux2){
+                    if(matriz[aux2-1][j+1]==0){
+                        j++;
+                        i=aux2-1;
+                    }
+                        else{
+                            i++;
+                        }
+                }
+                    else{
+                         if(matriz[aux2-1][0]==0){
+                             j=0;
+                             i=aux2-1;
+                        }
+                            else{
+                                i++;
+                            }
+                    }
+    }
+    aux+=(ordem*ordem)/4; i=ordem/2; j+=aux2;
+
+     while(num<=aux){
+        matriz[i][j]=num;
+        num++;
+        if(i-1>=aux2 && j+1<ordem){
+            if(matriz[i-1][j+1]==0){
+                i--;
+                j++;
+            }
+            else{
+                i++;
+            }
+        }
+            else if(i-1>=aux2){
+                if(matriz[i-1][aux2]==0){
+                   j=aux2;
+                   i--;
+                }
+                else{
+                    i++;
+                }
+            } 
+                else if(j+1<ordem){
+                    if(matriz[ordem-1][j+1]==0){
+                        j++;
+                        i=ordem-1;
+                    }
+                        else{
+                            i++;
+                        }
+                }
+                    else{
+                         if(matriz[ordem-1][aux2]==0){
+                             j=aux2;
+                             i=ordem-1;
+                        }
+                            else{
+                                i++;
+                            }
+                    }
+    }
+    aux+=(ordem*ordem)/4; i=0;
+
+     while(num<=aux){
+        matriz[i][j]=num;
+        num++;
+        if(i-1>=0 && j+1<ordem){
+            if(matriz[i-1][j+1]==0){
+                i--;
+                j++;
+            }
+            else{
+                i++;
+            }
+        }
+            else if(i-1>=0){
+                if(matriz[i-1][aux2]==0){
+                   j=aux2;
+                   i--;
+                }
+                else{
+                    i++;
+                }
+            } 
+                else if(j+1<ordem){
+                    if(matriz[aux2-1][j+1]==0){
+                        j++;
+                        i=aux2-1;
+                    }
+                        else{
+                            i++;
+                        }
+                }
+                    else{
+                         if(matriz[aux2-1][aux2]==0){
+                             j=aux2;
+                             i=aux2-1;
+                        }
+                            else{
+                                i++;
+                            }
+                    }
+    }
+
+    aux+=(ordem*ordem)/4; i=ordem/2; j=ordem/4;
+
+     while(num<=aux){
+        matriz[i][j]=num;
+        num++;
+        if(i-1>=aux2 && j+1<aux2){
+            if(matriz[i-1][j+1]==0){
+                i--;
+                j++;
+            }
+            else{
+                i++;
+            }
+        }
+            else if(i-1>=aux2){
+                if(matriz[i-1][0]==0){
+                   j=0;
+                   i--;
+                }
+                else{
+                    i++;
+                }
+            } 
+                else if(j+1<aux2){
+                    if(matriz[ordem-1][j+1]==0){
+                        j++;
+                        i=ordem-1;
+                    }
+                        else{
+                            i++;
+                        }
+                }
+                    else{
+                         if(matriz[ordem-1][0]==0){
+                             j=0;
+                             i=ordem-1;
+                        }
+                            else{
+                                i++;
+                            }
+                    }
+    }
+
+       for(i=0; i<aux2; i++)
+        for(j=0; j<aux2/2; j++){
+            aux3=matriz[i][j];
+            matriz[i][j]=matriz[i+aux2][j];
+            matriz[i+aux2][j]=aux3;
+        }
+
+        aux3=matriz[(aux2/2)][(aux2/2)];
+        matriz[(aux2/2)][(aux2/2)]=matriz[(aux2/2)+aux2][(aux2/2)];
+        matriz[(aux2/2)+aux2][(aux2/2)]=aux3;
+
+    printf("\n\nMatriz magica de ordem =%d:\n\n", ordem);
+    for (k = 0; k < ordem; k++){
+        for (l = 0; l < ordem; l++){
+            printf("%5d ", matriz[k][l]);
+        }
+        printf("\n");
+    }
+     printf("\n\nSoma de cada linha/coluna = %d\n\n", ordem * (ordem * ordem + 1) / 2);
 }
 
 /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
@@ -136,6 +330,7 @@ int main (void){
     setlocale(LC_ALL,"Portuguese");
 
     unsigned int ordem;
+    char ch;
 
     do{
         system(CLEAR);
@@ -159,16 +354,14 @@ int main (void){
         getchar();
     }
 
-    /*
+    
     else{
         matriz_par(ordem);
         printf("\nPressione ENTER para sair.");
         getchar();
         getchar();
     }
-    */
 
-    return 0;
 }
 
 /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
