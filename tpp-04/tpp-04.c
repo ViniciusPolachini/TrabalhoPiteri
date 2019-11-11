23623f962b774c40950ca9ce213d870ada6a6b9d
ViniciusPolachini <vipolachini@uol.com.br>
2019-11-11 00:07:47 -0300

    colocar palavras na horizontal

--- tpp-04/tpp-04.c
+++ tpp-04/tpp-04.c
@@ -20,6 +20,55 @@
 
 #define MAX 20
 
+/*void atualizaPalavras(char **palavras, int palavra, int x){
+    int i, j;
+    for (i=palavra; i<x; i++){
+        for(j=0; *(palavras[i+1]+j)!='\0'; j++){
+            *(palavras[i]+j) = *(palavras[i+1]+j);
+            printf("%d|%c\n", j,*(palavras[i]+j));
+        }
+        *(palavras[i]+j+1)='\0';
+       printf("\n|%d|\n\n", i);
+    }
+}*/
+
+void Random(int *num, int ini, int fim){
+    clock_t seed;
+    seed = clock();
+    srand(seed);
+    *(num) = rand() % (fim + 1 - ini) + ini;
+}
+
+void posicionaPalavra(char **palavras, char *matriz){
+    int i, j, NL,k=0, p[19], coluna, linha, palavra, aux;
+    for(i=0; i<20; i++) p[i]=20;
+    //Horizontal
+        for(i=0; i<4; i++){
+            do{
+                aux=0;
+                Random(&palavra, 0, 20);
+                for(j=0; j<k; j++){
+                    if(palavra==p[j]) aux=1;
+                }
+            }while(aux);
+            p[k]=palavra;
+            k++;
+            for(NL=0; *(palavras[palavra]+NL)!='\0';NL++);
+            Random(&coluna,0,19-NL);
+            Random(&linha,0,19);
+            printf("\nColuna:%d\nLinha:%d\nPalavara:%d\n\n", coluna, linha, palavra);
+            for(j=0; j<NL; j++){
+                *(matriz+linha*20+coluna+j)=*(palavras[palavra]+j);
+            }
+        }
+    //vertical
+
+    //diagonal principal
+
+    //diagonal secundária
+}
+
+
 /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 
 void inicializarmatriz(char *matriz){
@@ -59,13 +108,13 @@ void imprimirmatriz(char *matriz){
 /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 
 int main(void){
-
-    char *palavras[MAX] = {"BARCELONA", "BRATISLAVA", "CAMBRIDGE", "COPENHAGUE", "DUSSELDORF",
+    char *palavras[MAX] = {"BARCELONA ", "BRATISLAVA", "CAMBRIDGE", "COPENHAGUE", "DUSSELDORF",
     "EDIMBURGO", "FRANKFURT", "KIEV", "LIVERPOOL", "LEEDS", "MAASTRICHT", "MADRID", "MANCHESTER",
     "LISBOA", "LONDRES", "PALERMO", "PARIS", "SERVILHA", "SHEFFIED", "TURIM"};
 
     char matriz[MAX][MAX];
     inicializarmatriz(&matriz[0][0]);
+    posicionaPalavra(palavras, &matriz[0][0]);
     gerarmatriz(&matriz[0][0]);
     imprimirmatriz(&matriz[0][0]);
     return 0;
